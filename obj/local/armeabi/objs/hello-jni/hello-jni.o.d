532793816bc5972552ed2a47a5174045a54539ad
dkmeteor <93440331@qq.com>
2014-02-25 00:29:51 +0800

    update JNI

---
+++ obj/local/armeabi/objs/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs/hello-jni/hello-jni.o: jni/hello-jni.c \
+ D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/string.h \
+ D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/sys/cdefs.h \
+ D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/android/api-level.h \
+ D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/malloc.h \
+ D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/jni.h
+
+D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/string.h:
+
+D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/sys/cdefs.h:
+
+D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/android/api-level.h:
+
+D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/malloc.h:
+
+D:/WORK/android-ndk-r9-windows-x86_64/android-ndk-r9/platforms/android-18/arch-arm/usr/include/jni.h:
